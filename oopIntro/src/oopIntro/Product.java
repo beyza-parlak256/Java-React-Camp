73e2aaf00d7dd1099078e307c9548132805df383
Beyza Parlak <beyza.parlak98@erzurum.edu.tr>
2021-05-14 13:53:02 +0300

    +Java - React Camp 3 day codes

--- oopIntro/src/oopIntro/Product.java
+++ oopIntro/src/oopIntro/Product.java
@@ -2,23 +2,75 @@ package oopIntro;
 
 public class Product {
 	
-	// Constructor Block
-	public Product(){
-		System.out.println("Ben calistim");
+	// encapsulation
+	// final sadece constructorda set edilebilir demek
+	
+	private int id;
+	private String name;
+	private String detail;
+	private double unitPrice;
+	private double discount; 
+	
+	
+	public Product() {
+		
 	}
 	
-	public Product(int id, String name, double unitPrice, String detail){
-		this();
+	
+	public Product(int id, String name, String detail, double unitPrice, double discount, double unitPriceAfterDiscount) {
+		super();
 		this.id = id;
 		this.name = name;
+		this.detail = detail;
 		this.unitPrice = unitPrice;
+		this.discount = discount;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
 		this.detail = detail;
 	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public double getUnitPriceAfterDiscount() {
+		return this.getUnitPriceAfterDiscount() - (this.unitPrice * this.discount / 100);
+	}
 	
-	int id;
-	String name;
-	String detail;
-	double unitPrice;
 	
 }
 
--- oopIntro/src/oopIntro/ProductManager.java
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -4,14 +4,11 @@ public class ProductManager {
 
 	public void addToCart(Product product){
 		
-		System.out.println("Sepete eklendi : " + product.name);
+		System.out.println("Sepete eklendi : " + product.getName());
 		
 	}
 	
 	
 }
-
-
-
-
+																				
 
